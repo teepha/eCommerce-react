8aaccebcbeb3e44fde591631dab9e769d07f7b77
SasaTuring <sasa.m@turing.com>
2019-06-26 00:26:57 +0200

    re-constructing and re-styling product items

--- src/components/ListProduct/index.js
+++ src/components/ListProduct/index.js
@@ -5,6 +5,7 @@ import {withStyles} from '@material-ui/core';
 import classNames from 'classnames';
 import {Link} from 'react-router-dom';
 import styles from './styles';
+import './styles.css';
 
 class ListProduct extends Component {
 
@@ -13,27 +14,35 @@ class ListProduct extends Component {
         const isDiscounted = parseFloat(product.discounted_price) > 0;
 
         return (
-            <div className="max-w-sm overflow-hidden shadow-lg bg-white w-11/12 justify-center relative product-card">
+            <div className="max-w-sm overflow-hidden w-11/12 justify-center relative product-card">
 
-                {parseFloat(product.discounted_price) > 0 &&
-                <div className={classes.wasBlockContainer}>
-                    <span className={classes.wasBlock}>SALE</span>
-                </div>}
+                <div className="product-info-block">
 
-                <img className="w-full" src={systemConfig.imageBaseUrl + product.thumbnail} alt="Product Image"/>
+                  {parseFloat(product.discounted_price) > 0 &&
+                  <div className={classes.wasBlockContainer}>
+                      <span className={classes.wasBlock}>SALE</span>
+                  </div>}
 
-                <div className="py-2 text-center">
-                   <span className={`product-card-title ${classes.productTitle}`}>
-                        {product.name}
-                    </span>
-                </div>
+                  <img className="w-full" src={systemConfig.imageBaseUrl + product.thumbnail} alt="Product Image"/>
 
-                <div className="py-2 text-center">
-                     <span className={classes.productPrice}>
-                        <span className={classNames({
-                            [classes.strikeThrough]: isDiscounted
-                        })}>£ {product.price}</span>{ isDiscounted && <span> | £ {product.discounted_price}</span>}
-                </span>
+                  <div className="productTextDiv">
+
+                    <div>
+                       <span className={`product-card-title ${classes.productTitle}`}>
+                            {product.name}
+                        </span>
+                    </div>
+
+                    <div>
+                         <span className={classes.productPrice}>
+                            <span className={classNames({
+                                [classes.strikeThrough]: isDiscounted
+                            })}>£ {product.price}</span>{ isDiscounted && <span> | £ {product.discounted_price}</span>}
+                            </span>
+                    </div>
+
+                  </div>
+
                 </div>
 
                 <Link to={`/product/${product.product_id}`}>
@@ -50,4 +59,3 @@ class ListProduct extends Component {
 
 
 export default withStyles(styles)(ListProduct);
-
